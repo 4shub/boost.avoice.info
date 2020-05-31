dcf0b380f6353a1411adb9d0dc6c0e88eadd8c68
Audrey McNay <amcnay@utexas.edu>
2020-05-30 22:46:38 -0700

    editing content

--- index.js
+++ index.js
@@ -267,7 +267,7 @@ const MoreWaysToHelp = () => (
 
 const Footer = () => (
     <section className="footer">
-        We made this website to amplify the voices of the BLM movement, and we plan to add more functionalities in the near future. If you have any feature suggestions or resources to share, please DM <a rel="noreferrer" target="_blank" href="https://twitter.com/oddrilynn">@oddrilynn</a> or<a rel="noreferrer" target="_blank" href="https://twitter.com/4shub">@4shub</a> on Twitter.
+        We made this website to amplify the voices of the BLM movement, and we plan to add more functionalities in the near future. If you have any feature suggestions or resources to share, please DM <a rel="noreferrer" target="_blank" href="https://twitter.com/oddrilynn">@oddrilynn</a> or <a rel="noreferrer" target="_blank" href="https://twitter.com/4shub">@4shub</a> on Twitter.
     </section>
 )
 
