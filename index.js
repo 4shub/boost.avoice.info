3417a7f2ffeba49cde670dae143d2a848eb9eddf
Shubham Naik <shub@shub.club>
2020-05-31 21:51:26 -0700

    chore: update

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const ALL_US_STATES = ["Alaska",
     "Mississippi",
     "Montana",
     "North Carolina",
-    " North Dakota",
+    "North Dakota",
     "Nebraska",
     "New Hampshire",
     "New Jersey",
@@ -52,7 +52,262 @@ const ALL_US_STATES = ["Alaska",
     "Washington",
     "Wisconsin",
     "West Virginia",
-    "Wyoming"]
+    "Wyoming"];
+
+const USA = 'United States of America';
+
+const COUNTRY_LIST = [
+    USA,
+    "Canada",
+    "Afghanistan",
+    "Albania",
+    "Algeria",
+    "American Samoa",
+    "Andorra",
+    "Angola",
+    "Anguilla",
+    "Antarctica",
+    "Antigua and Barbuda",
+    "Argentina",
+    "Armenia",
+    "Aruba",
+    "Australia",
+    "Austria",
+    "Azerbaijan",
+    "Bahamas",
+    "Bahrain",
+    "Bangladesh",
+    "Barbados",
+    "Belarus",
+    "Belgium",
+    "Belize",
+    "Benin",
+    "Bermuda",
+    "Bhutan",
+    "Bolivia",
+    "Bonaire, Sint Eustatius and Saba",
+    "Bosnia and Herzegovina",
+    "Botswana",
+    "Bouvet Island",
+    "Brazil",
+    "British Indian Ocean Territory",
+    "Brunei Darussalam",
+    "Bulgaria",
+    "Burkina Faso",
+    "Burundi",
+    "Cabo Verde",
+    "Cambodia",
+    "Cameroon",
+    "Cayman Islands",
+    "Central African Republic",
+    "Chad",
+    "Chile",
+    "China",
+    "Christmas Island",
+    "Cocos (Keeling) Islands",
+    "Colombia",
+    "Comoros",
+    "Democratic Republic of the Congo",
+    "Congo",
+    "Cook Islands",
+    "Costa Rica",
+    "Croatia",
+    "Cuba",
+    "Curaçao",
+    "Cyprus",
+    "Czechia",
+    "Côte d'Ivoire",
+    "Denmark",
+    "Djibouti",
+    "Dominica",
+    "Dominican Republic",
+    "Ecuador",
+    "Egypt",
+    "El Salvador",
+    "Equatorial Guinea",
+    "Eritrea",
+    "Estonia",
+    "Eswatini",
+    "Ethiopia",
+    "Falkland Islands [Malvinas]",
+    "Faroe Islands",
+    "Fiji",
+    "Finland",
+    "France",
+    "French Guiana",
+    "French Polynesia",
+    "French Southern Territories",
+    "Gabon",
+    "Gambia",
+    "Georgia",
+    "Germany",
+    "Ghana",
+    "Gibraltar",
+    "Greece",
+    "Greenland",
+    "Grenada",
+    "Guadeloupe",
+    "Guam",
+    "Guatemala",
+    "Guernsey",
+    "Guinea",
+    "Guinea-Bissau",
+    "Guyana",
+    "Haiti",
+    "Heard Island and McDonald Islands",
+    "Holy See",
+    "Honduras",
+    "Hong Kong",
+    "Hungary",
+    "Iceland",
+    "India",
+    "Indonesia",
+    "Iran",
+    "Iraq",
+    "Ireland",
+    "Isle of Man",
+    "Israel",
+    "Italy",
+    "Jamaica",
+    "Japan",
+    "Jersey",
+    "Jordan",
+    "Kazakhstan",
+    "Kenya",
+    "Kiribati",
+    "North Korea",
+    "South Korea",
+    "Kuwait",
+    "Kyrgyzstan",
+    "Lao People's Democratic Republic",
+    "Latvia",
+    "Lebanon",
+    "Lesotho",
+    "Liberia",
+    "Libya",
+    "Liechtenstein",
+    "Lithuania",
+    "Luxembourg",
+    "Macao",
+    "Madagascar",
+    "Malawi",
+    "Malaysia",
+    "Maldives",
+    "Mali",
+    "Malta",
+    "Marshall Islands",
+    "Martinique",
+    "Mauritania",
+    "Mauritius",
+    "Mayotte",
+    "Mexico",
+    "Micronesia",
+    "Moldova",
+    "Monaco",
+    "Mongolia",
+    "Montenegro",
+    "Montserrat",
+    "Morocco",
+    "Mozambique",
+    "Myanmar",
+    "Namibia",
+    "Nauru",
+    "Nepal",
+    "Netherlands",
+    "New Caledonia",
+    "New Zealand",
+    "Nicaragua",
+    "Niger",
+    "Nigeria",
+    "Niue",
+    "Norfolk Island",
+    "Northern Mariana Islands",
+    "Norway",
+    "Oman",
+    "Pakistan",
+    "Palau",
+    "Palestine",
+    "Panama",
+    "Papua New Guinea",
+    "Paraguay",
+    "Peru",
+    "Philippines",
+    "Pitcairn",
+    "Poland",
+    "Portugal",
+    "Puerto Rico",
+    "Qatar",
+    "Republic of North Macedonia",
+    "Romania",
+    "Russian Federation",
+    "Rwanda",
+    "Réunion",
+    "Saint Barthélemy",
+    "Saint Helena, Ascension and Tristan da Cunha",
+    "Saint Kitts and Nevis",
+    "Saint Lucia",
+    "Saint Martin",
+    "Saint Pierre and Miquelon",
+    "Saint Vincent and the Grenadines",
+    "Samoa",
+    "San Marino",
+    "Sao Tome and Principe",
+    "Saudi Arabia",
+    "Senegal",
+    "Serbia",
+    "Seychelles",
+    "Sierra Leone",
+    "Singapore",
+    "Sint Maarten",
+    "Slovakia",
+    "Slovenia",
+    "Solomon Islands",
+    "Somalia",
+    "South Africa",
+    "South Georgia and the South Sandwich Islands",
+    "South Sudan",
+    "Spain",
+    "Sri Lanka",
+    "Sudan",
+    "Suriname",
+    "Svalbard and Jan Mayen",
+    "Sweden",
+    "Switzerland",
+    "Syrian Arab Republic",
+    "Taiwan",
+    "Tajikistan",
+    "Tanzania, United Republic of",
+    "Thailand",
+    "Timor-Leste",
+    "Togo",
+    "Tokelau",
+    "Tonga",
+    "Trinidad and Tobago",
+    "Tunisia",
+    "Turkey",
+    "Turkmenistan",
+    "Turks and Caicos Islands",
+    "Tuvalu",
+    "Uganda",
+    "Ukraine",
+    "United Arab Emirates",
+    "United Kingdom of Great Britain and Northern Ireland",
+    "United States Minor Outlying Islands",
+    "Uruguay",
+    "Uzbekistan",
+    "Vanuatu",
+    "Venezuela",
+    "Viet Nam",
+    "Virgin Islands (British)",
+    "Virgin Islands (U.S.)",
+    "Wallis and Futuna",
+    "Western Sahara",
+    "Yemen",
+    "Zambia",
+    "Zimbabwe",
+    "Åland Islands"
+];
+
 
 const EMAIL_DATA = {
     district_attorney: {
@@ -135,17 +390,17 @@ const buildMailTo = ({ email, body, subject }) => `mailto:${email}?subject=${enc
 
 const emailDataEntries = Object.entries(EMAIL_DATA);
 
-const applyPreview = (previewType, { firstName, lastName, city, state }) => {
-
+const applyPreview = (previewType, { firstName, lastName, city, state, country }) => {
+    const region = country === USA ? state : country;
 
     return EMAIL_DATA[previewType] && EMAIL_DATA[previewType].body({
-        firstName: firstName || 'FIRST NAME', lastName: lastName ? ` ${lastName}` : '', city: city || 'CITY', state: state || 'state'
+        firstName: firstName || 'FIRST NAME', lastName: lastName ? ` ${lastName}` : '', city: city || 'CITY', state: region || 'state'
     });
 }
 
 const EmailData = () => {
-    const [emailInfo , updateState] = React.useState({ state: 'Alaska'  });
-    const { firstName, lastName, city, state } = emailInfo;
+    const [emailInfo , updateState] = React.useState({ state: 'Alaska', country: USA  });
+    const { firstName, lastName, city, state, country } = emailInfo;
     const [previewType, onUpdatePreview] = React.useState(null);
     const [step, setNextStep] = React.useState(0);
 
@@ -182,15 +437,24 @@ const EmailData = () => {
                     <input className="input-item" name="lastName" value={lastName} placeholder="Your Last Name" onChange={setState} />
                 </label>
                 <label>
+                    Your Country
+                    <select required className="input-item" name="country"  value={country} onChange={setState}>
+                        {COUNTRY_LIST.map(a_country => <option key={a_country}>{a_country}</option>)}
+                    </select>
+                </label>
+                {country === USA && (
+                    <label>
+                        Your State
+                        <select required className="input-item" name="state"  value={state} onChange={setState}>
+                            {ALL_US_STATES.map(a_state => <option key={a_state}>{a_state}</option>)}
+                        </select>
+                    </label>
+                )}
+                <label>
                     Your City
                     <input required className="input-item" name="city" value={city} placeholder="Your City" onChange={setState} />
                 </label>
-                <label>
-                    Your State
-                    <select required className="input-item" name="state"  value={state} onChange={setState}>
-                        {ALL_US_STATES.map(a_state => <option key={a_state}>{a_state}</option>)}
-                    </select>
-                </label>
+
                 <button type="submit" className="button">
                     Prep My Email
                 </button>
