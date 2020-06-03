ab680756bcc05c2f99cfe74ffbebc5866cd56e8e
Shubham Naik <shub@shub.club>
2020-06-02 21:29:24 -0700

    fix config

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,8 @@ TMP_PATH="$PWD/../tmp-dock-pkg.tar.gz";
 
 rm -rf $TMP_PATH
 
+rm -rf node_modules
+
 (cd $PWD && tar -X $PWD/.platformignore -czvf $TMP_PATH .)
 
 curl -F "data=@$TMP_PATH" --header "Authorization: Bearer $DEPLOY_TOKEN" "https://platform.shub.dev/app/boost?type=production"
