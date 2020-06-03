dec25d832b8e78ac58877c263dff3d22d5bd8927
Shubham Naik <shub@shub.club>
2020-06-02 21:47:09 -0700

    chore: add github link

--- src/app/footer/footer.tsx
+++ src/app/footer/footer.tsx
@@ -1,19 +1,27 @@
-import React from "react";
+import React from 'react';
 
 const Footer = () => (
-    <section className="footer">
-        We made this website to amplify the voices of the BLM movement, and we plan to add more functionalities in the
-        near future. If you have any feature suggestions or resources to share, please DM{' '}
-        <a rel="noreferrer" target="_blank" href="https://twitter.com/oddrilynn">
-            @oddrilynn
-        </a>{' '}
-        or{' '}
-        <a rel="noreferrer" target="_blank" href="https://twitter.com/4shub">
-            @4shub
-        </a>{' '}
-        on Twitter.
-    </section>
+    <React.Fragment>
+        <footer className="footer">
+            We made this website to amplify the voices of the BLM movement, and we plan to add more functionalities in
+            the near future. If you have any feature suggestions or resources to share, please DM{' '}
+            <a rel="noreferrer" target="_blank" href="https://twitter.com/oddrilynn">
+                @oddrilynn
+            </a>{' '}
+            or{' '}
+            <a rel="noreferrer" target="_blank" href="https://twitter.com/4shub">
+                @4shub
+            </a>{' '}
+            on Twitter.
+        </footer>
+        <footer className="footer">
+            This code is open source (MIT), feel free to look through the code{' '}
+            <a rel="noreferrer" target="_blank" href="https://github.com/4shub/boost.avoice.info">
+                here
+            </a>
+            .
+        </footer>
+    </React.Fragment>
 );
-
 
 export default Footer;
