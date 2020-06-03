0971672ec12e150f6f2de20464f20d0da522fde7
Audrey McNay <amcnay@utexas.edu>
2020-06-03 14:58:17 -0700

    hotfix: link rendering

--- src/app/footer/footer.tsx
+++ src/app/footer/footer.tsx
@@ -4,14 +4,14 @@ const Footer = () => (
     <React.Fragment>
         <footer className="footer">
             We made this website to amplify the voices of the BLM movement, and we plan to add more functionalities in
-            the near future. If you have any feature suggestions or resources to share, please DM{' '}
+            the near future. If you have any feature suggestions or resources to share, please DM<span>{' '}
             <a rel="noreferrer" target="_blank" href="https://twitter.com/oddrilynn">
                 @oddrilynn
-            </a>{' '}
-            or{' '}
+            </a>{' '}</span>
+            or<span>{' '}
             <a rel="noreferrer" target="_blank" href="https://twitter.com/4shub">
                 @4shub
-            </a>{' '}
+            </a>{' '}</span>
             on Twitter.
         </footer>
         <footer className="footer">
