3546fe41e1fd0734903639ba41d28bfb06c75ef0
Ryan Yoon Bum Choi <ryanychoi90@gmail.com>
2020-06-03 21:58:19 -0700

    feat:Add mobile check and gmail URL scheme

--- src/app/email-processor/email-processor.tsx
+++ src/app/email-processor/email-processor.tsx
@@ -6,7 +6,15 @@ import { EmailProcessorProps } from './email-processor.types';
 import { TemplateDataGroup, TemplateGroup } from '../../template-builder/template.types';
 import SendEmailToSelector from './send-email-to-selector/send-email-to-selector';
 
+const isMobile = () => 
+    (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+
 const buildGmailLink = ({ email, body, subject }) =>
+    isMobile() ?
+    `googlegmail:///co?to=${email}&subject=${encodeURIComponent(
+        subject
+    )}&body=${encodeURIComponent(body)}`
+    :
     `https://mail.google.com/mail/u/0/?view=cm&fs=1&tf=1&to=${email}&su=${encodeURIComponent(
         subject
     )}&body=${encodeURIComponent(body)}`;
